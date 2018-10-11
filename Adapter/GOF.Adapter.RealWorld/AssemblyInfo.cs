b4b71cb95e0d8b9dc6312558e824598402ab6621
m4ni5h <manish.kumar5139@gmail.com>
2018-10-11 23:01:46 +0530

    A close to world example for the Adapter pattern

---
+++ Adapter/GOF.Adapter.RealWorld/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Gang Of Four Design Patterns")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("4.0.*")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
