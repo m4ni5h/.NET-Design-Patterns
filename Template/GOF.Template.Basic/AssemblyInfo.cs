91e898678c65303ab7a5a85eaf474b3dfc6aab87
m4ni5h <manish.kumar5139@gmail.com>
2018-11-04 23:27:16 +0530

    A Basic and A Real World example of Template Design Pattern

---
+++ Template/GOF.Template.Basic/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Gang Of Four Design Patterns")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("4.0.*")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
