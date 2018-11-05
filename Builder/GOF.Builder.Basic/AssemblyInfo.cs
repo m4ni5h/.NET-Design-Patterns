67c29d02280f76458307a9fb41b1b64e85257d70
m4ni5h <manish.kumar5139@gmail.com>
2018-11-05 21:09:36 +0530

    A Basic and Real world example of Builder Design pattern.

---
+++ Builder/GOF.Builder.Basic/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Gang Of Four Design Patterns")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("4.0.*")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
